794dc5e3273ebaddb8bc3cd2f4ff859ae9754e37
Vincent Lauro <vinniehat@gmail.com>
2022-09-13 11:08:52 -0400

    Initial Project

---
+++ PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeNumbersAgain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
